39674f40cbc846a6682613f66cb647a445a23d3e
Adam W <GModModGod@users.noreply.github.com>
2017-06-23 16:41:32 -0400

    Update menumods_init.lua

--- menu_mods/lua/autorun/menu/menumods_init.lua
+++ menu_mods/lua/autorun/menu/menumods_init.lua
@@ -1,29 +1,41 @@
 
-if MenuMods_Initialized then return end
+if (not MenuMods_Initialized) then
+	local exec = CompileString(file.Read("lua/autorun/menu/menumods_menu.lua", "GAME"), "autorun/menu/menumods_menu.lua", true)
 
-local exec = CompileString(file.Read("lua/autorun/menu/menumods_menu.lua", "GAME"), "autorun/menu/menumods_menu.lua", true)
+	exec()
+end
 
-exec()
+local FileTable = {}
 
-local files, dirs = file.Find("lua/autorun/menu/*.lua", "GAME")
+local function Mount()
+	local files, dirs = file.Find("lua/autorun/menu/*.lua", "GAME")
 
-for k, v in pairs(files) do
-	if (not dirs[k]) then
-		dirs[k] = "lua/autorun/menu"
-	end
-	
-	local filename = (dirs[k] .. "/" .. v)
-	
-	if file.Exists(filename, "GAME") then
-		if ((filename != "lua/autorun/menu/menumods_init.lua") and (filename != "lua/autorun/menu/menumods_menu.lua")) then
-			local exec = CompileString(file.Read(filename, "GAME"), string.TrimLeft(filename, "lua/"), true)
-			local function handleError(err)
-				print("[ERROR] " .. err)
+	for k, v in pairs(files) do
+		if (not dirs[k]) then
+			dirs[k] = "lua/autorun/menu"
+		end
+		
+		local filename = (dirs[k] .. "/" .. v)
+		
+		if (not FileTable[filename]) then
+			if file.Exists(filename, "GAME") then
+				if ((filename != "lua/autorun/menu/menumods_init.lua") and (filename != "lua/autorun/menu/menumods_menu.lua")) then
+					local exec = CompileString(file.Read(filename, "GAME"), string.TrimLeft(filename, "lua/"), true)
+					local function handleError(err)
+						print("[ERROR] " .. err)
+					end
+					
+					xpcall(exec, handleError)
+					
+					FileTable[filename] = true
+				end
 			end
-			
-			xpcall(exec, handleError)
 		end
 	end
 end
+
+Mount()
+
+hook.Add("GameContentChanged", "MenuMods_GameContentChanged", Mount)
 
 MenuMods_Initialized = true
