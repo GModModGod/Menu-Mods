927702702e3e2aa28e9cd42af9be15c792292135
Adam W <GModModGod@users.noreply.github.com>
2017-06-24 12:00:42 -0400

    Update menumods.js

--- menu_mods/html/js/menu/menumods.js
+++ menu_mods/html/js/menu/menumods.js
@@ -1,19 +1,2 @@
 
-var menumods = {
-	hook: {
-		Run: function(identifier, args)
-		{
-			var currString = "menumods.hook.Run(\"" + identifier + "\""
-			
-			for(i = 0; i < args.length; i++) {
-				currString += ", \"" + args[i] + "\""
-			}
-			
-			currString += ")"
-			
-			lua.Run(currString)
-		}
-	}
-}
-
 lua.Run("local exec = CompileString(file.Read(\"lua/autorun/menu/menumods_init.lua\", \"GAME\"), \"autorun/menu/menumods_init.lua\", true); exec()")
