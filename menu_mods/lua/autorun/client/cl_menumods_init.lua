044ff81bd68b0b36eaa3a96eda139e7efdab5dd1
Adam W <GModModGod@users.noreply.github.com>
2017-11-04 17:36:15 -0400

    Delete cl_menumods_init.lua

--- menu_mods/lua/autorun/client/cl_menumods_init.lua
+++
@@ -1,8 +0,0 @@
-
-local function Initialize()
-	if (pnlMainMenu and pnlMainMenu:IsValid() and pnlMainMenu.HTML) then
-		pnlMainMenu.HTML.ShouldRefresh = true
-	end
-end
-
-hook.Add("MenuMods_Initialize", Initialize)
