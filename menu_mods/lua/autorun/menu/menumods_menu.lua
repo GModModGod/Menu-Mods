150297c0bc715378ec9362ce628f09d616930e5a
Adam W <GModModGod@users.noreply.github.com>
2017-11-21 12:08:52 -0500

    Add files via upload

--- menu_mods/lua/autorun/menu/menumods_menu.lua
+++ menu_mods/lua/autorun/menu/menumods_menu.lua
@@ -736,6 +736,7 @@ pnlMainMenu.Think = function(self)
 end
 
 menumods.include("includes/modules/luahtml.lua")
+menumods.include("includes/modules/luajs.lua")
 
 local files, dirs = file.Find("lua/htmldocs/*.lua", "GAME")
 
@@ -781,9 +782,65 @@ for k, v in ipairs(files) do
 	end
 end
 
-LUA_HTML = {}
+LUA_HTML = nil
 
 for k, v in pairs(luahtml.GetClasses()) do
+	if istable(v.BaseClass) then
+		for i, j in pairs(v.BaseClass) do
+			if (v[i] == nil) then
+				v[i] = j
+			end
+		end
+	end
+end
+
+local files, dirs = file.Find("lua/jsdocs/*.lua", "GAME")
+
+for k, v in ipairs(files) do
+	local startPos, endPos = string.find(v, "%.lua$", 1, false)
+	
+	if (not dirs[k]) then
+		dirs[k] = "lua/jsdocs"
+	end
+	
+	if startPos then
+		local name = string.sub(v, 1, (startPos - 1))
+		
+		local shouldMount = true
+		
+		local dirTab = string.Explode("/", dirs[k], false)
+		
+		if ((#dirTab == 3) and (name == "init")) then
+			name = string.lower(dirTab[#dirTab])
+		elseif (#dirTab != 2) then
+			shouldMount = false
+		end
+		
+		if shouldMount then
+			local fullPath = string.gsub((dirs[k] .. "/" .. v), "^lua/", "", 1)
+			
+			LUA_JS = {}
+			
+			LUA_JS.ClassName = name
+			
+			if SERVER then
+				AddCSLuaFile(fullPath)
+			end
+			
+			menumods.include(fullPath)
+			
+			if (not LUA_JS.Base) then
+				LUA_JS.Base = "base_js"
+			end
+			
+			luajs.Register(LUA_JS, name)
+		end
+	end
+end
+
+LUA_JS = nil
+
+for k, v in pairs(luajs.GetClasses()) do
 	if istable(v.BaseClass) then
 		for i, j in pairs(v.BaseClass) do
 			if (v[i] == nil) then
