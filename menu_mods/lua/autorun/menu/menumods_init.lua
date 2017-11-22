2bea0b72b80175710e95ecd01a080d9f2c1181d4
Adam W <GModModGod@users.noreply.github.com>
2017-11-21 21:19:06 -0500

    Add files via upload

--- menu_mods/lua/autorun/menu/menumods_init.lua
+++ menu_mods/lua/autorun/menu/menumods_init.lua
@@ -73,6 +73,8 @@ if (not MenuMods_Initialized) then
 end
 
 local FileTable = {}
+local HTMLFileTable = {}
+local JSFileTable = {}
 
 local function Mount()
 	local files, dirs = file.Find("lua/autorun/menu/*.lua", "GAME")
@@ -100,6 +102,130 @@ local function Mount()
 		end
 	end
 	
+	local files, dirs = file.Find("lua/htmldocs/*.lua", "GAME")
+
+	for k, v in ipairs(files) do
+		if (not dirs[k]) then
+			dirs[k] = "lua/htmldocs"
+		end
+		
+		local filename = (dirs[k] .. "/" .. v)
+		
+		if (not HTMLFileTable[filename]) then
+			local startPos, endPos = string.find(v, "%.lua$", 1, false)
+			
+			if startPos then
+				local name = string.sub(v, 1, (startPos - 1))
+				
+				local shouldMount = true
+				
+				local dirTab = string.Explode("/", dirs[k], false)
+				
+				if ((#dirTab == 3) and (name == "init")) then
+					name = string.lower(dirTab[#dirTab])
+				elseif (#dirTab != 2) then
+					shouldMount = false
+				end
+				
+				if shouldMount then
+					local fullPath = string.gsub((dirs[k] .. "/" .. v), "^lua/", "", 1)
+					
+					LUA_HTML = {}
+					
+					LUA_HTML.ClassName = name
+					
+					if SERVER then
+						AddCSLuaFile(fullPath)
+					end
+					
+					menumods.include(fullPath)
+					
+					if (not LUA_HTML.Base) then
+						LUA_HTML.Base = "base_html"
+					end
+					
+					luahtml.Register(LUA_HTML, name)
+				end
+			end
+			
+			HTMLFileTable[filename] = true
+		end
+	end
+
+	LUA_HTML = nil
+
+	for k, v in pairs(luahtml.GetClasses()) do
+		if istable(v.BaseClass) then
+			for i, j in pairs(v.BaseClass) do
+				if (v[i] == nil) then
+					v[i] = j
+				end
+			end
+		end
+	end
+
+	local files, dirs = file.Find("lua/jsdocs/*.lua", "GAME")
+
+	for k, v in ipairs(files) do
+		if (not dirs[k]) then
+			dirs[k] = "lua/jsdocs"
+		end
+		
+		local filename = (dirs[k] .. "/" .. v)
+		
+		if (not JSFileTable[filename]) then
+			local startPos, endPos = string.find(v, "%.lua$", 1, false)
+			
+			if startPos then
+				local name = string.sub(v, 1, (startPos - 1))
+				
+				local shouldMount = true
+				
+				local dirTab = string.Explode("/", dirs[k], false)
+				
+				if ((#dirTab == 3) and (name == "init")) then
+					name = string.lower(dirTab[#dirTab])
+				elseif (#dirTab != 2) then
+					shouldMount = false
+				end
+				
+				if shouldMount then
+					local fullPath = string.gsub((dirs[k] .. "/" .. v), "^lua/", "", 1)
+					
+					LUA_JS = {}
+					
+					LUA_JS.ClassName = name
+					
+					if SERVER then
+						AddCSLuaFile(fullPath)
+					end
+					
+					menumods.include(fullPath)
+					
+					if (not LUA_JS.Base) then
+						LUA_JS.Base = "base_js"
+					end
+					
+					luajs.Register(LUA_JS, name)
+				end
+			end
+			
+			JSFileTable[filename] = true
+		end
+	end
+
+	LUA_JS = nil
+
+	for k, v in pairs(luajs.GetClasses()) do
+		if istable(v.BaseClass) then
+			for i, j in pairs(v.BaseClass) do
+				if (v[i] == nil) then
+					v[i] = j
+				end
+			end
+		end
+	end
+
 	if MenuMods_Initialized then
 		menumods.hook.Run("Initialize")
 	end
