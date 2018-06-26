b51809ca5a54c5528e1f919f522ef4fe9690bc7e
Adam W <GModModGod@users.noreply.github.com>
2018-06-26 01:49:35 -0400

    Add files via upload

--- menu_mods/html/js/menu/menumods.js
+++ menu_mods/html/js/menu/menumods.js
@@ -66,15 +66,16 @@ var menumods =
 				var escChars = menumods.string.escChars;
 				
 				for (k in escChars) {
-					var currStr = menumods.string.patternSafe(escChars[k][0])
+					var currStr1 = menumods.string.patternSafe(escChars[k][0]);
+					var currStr2 = menumods.string.patternSafe(escChars[k][1]);
 					
-					var pattern1 = new RegExp(("^" + currStr), "g");
-					var pattern2 = new RegExp(("([^\\\\])" + currStr), "g");
-					var pattern3 = new RegExp(("\\\\" + menumods.string.patternSafe(escChars[k][1])), "g");
+					var pattern1 = new RegExp(currStr1, "g");
+					var pattern2 = new RegExp(("^\\\\" + currStr2), "g");
+					var pattern3 = new RegExp(("([^\\\\])\\\\" + currStr2), "g");
 					
 					newString = newString.replace(pattern1, "");
-					newString = newString.replace(pattern2, "$1");
-					newString = newString.replace(pattern3, currStr);
+					newString = newString.replace(pattern2, currStr1)
+					newString = newString.replace(pattern3, ("$1" + currStr1));
 				}
 				
 				newString = newString.replace(/\\\\/g, "\\");
