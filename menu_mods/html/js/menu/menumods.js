bafe68646563d21e2b58a215040c7b6186a547be
Adam W <GModModGod@users.noreply.github.com>
2018-07-04 00:10:50 -0400

    Add files via upload

--- menu_mods/html/js/menu/menumods.js
+++ menu_mods/html/js/menu/menumods.js
@@ -85,5 +85,3 @@ var menumods =
 		}
 	}
 }
-
-lua.Run("local exec = CompileString(file.Read(\"lua/autorun/menu/menumods_init.lua\", \"GAME\"), \"autorun/menu/menumods_init.lua\", true); exec()")
---
+++ menu_mods/html/js/menu/menumods_init.js
@@ -0,0 +1,2 @@
+
+lua.Run("local exec = CompileString(file.Read(\"lua/autorun/menu/menumods_init.lua\", \"GAME\"), \"autorun/menu/menumods_init.lua\", true); exec()")
